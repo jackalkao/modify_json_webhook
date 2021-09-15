4a23390be2536ada52c99a3d28f659746b9d6d7c
jackalkao <jackal.kao@splashtop.com>
2021-09-15 18:41:37 +0800

    get json name by env

--- modify_json.sh
+++ modify_json.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 webhook_url=$1
+jaso_file=$2
 [ -z "$webhook_url" ] && {
   echo -e "\033[41;37mPlease set webhook url\033[0m"
   exit
@@ -10,7 +11,7 @@ while true
 do
  for n in $num
  do
-  output_warning_type=`cat brakeman-output.json | jq '.warnings' | jq ".[$n]"`
+  output_warning_type=`cat $jaso_file | jq '.warnings' | jq ".[$n]"`
   [ -z "$output_warning_type" -o "$output_warning_type" = "null" ] &&
   {
    break
@@ -21,14 +22,14 @@ do
   echo '],' >> brakeman-output-"$n".json
 
   ###update the commuit info###
-  git_project_path=`cat brakeman-output.json | grep app_path | awk -F'"' '{print $4}'`
+  git_project_path=`cat $jaso_file | grep app_path | awk -F'"' '{print $4}'`
   git_branch=`cd $git_project_path && git log -1 --pretty=format:%d | awk -F"," '{print $2}' | awk -F"origin/" '{print $2}' | awk -F")" '{print $1}'`
   git_commit_info=`cd $git_project_path && git log -1 --pretty=format:'  "git_commit": [%n    {%n      "id": "%H",%n      "message": "%s",%n      "notes": "%N",%n      "name": "%aN",%n      "email": "%aE",%n      "date": "%aD",%n      "branch": "'$git_branch'"%n    }%n  ],%n'`
   echo "$git_commit_info" >> brakeman-output-"$n".json
 
 echo $git_branch
   ###update repo info###
-  repo=`cat brakeman-output.json | grep app_path | awk -F'"' '{print $4}' | awk -F'/' '{print $4}'`
+  repo=`cat $jaso_file | grep app_path | awk -F'"' '{print $4}' | awk -F'/' '{print $4}'`
   echo '"repo_name": "'$repo'"' >> brakeman-output-"$n".json
   echo '}' >> brakeman-output-"$n".json
   curl -X POST $webhook_url -H "Content-Type:\ application/json" -d @"brakeman-output-"$n".json"
