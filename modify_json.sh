a54a0534094bda5be50c443146aa4421d86f1d19
jackal.kao <jackal.kao@splashtop.com>
2021-09-16 16:20:07 +0800

    open curl function

--- modify_json.sh
+++ modify_json.sh
@@ -39,7 +39,7 @@ do
   ###update repo info###
   echo '"repo_name": "'$repo'"' >> brakeman-output-"$n".json
   echo '}' >> brakeman-output-"$n".json
-  #curl -X POST $webhook_url -H "Content-Type:\ application/json" -d @"brakeman-output-"$n".json"
+  curl -X POST $webhook_url -H "Content-Type:\ application/json" -d @"brakeman-output-"$n".json"
   num=$(( $n + 1 ))
  done
   [ -z "$output_warning_type" -o "$output_warning_type" = "null" ] &&
