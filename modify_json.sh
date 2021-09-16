464170185e0a5b806d80c5ed64650f1e3d353e0d
jackal.kao <jackal.kao@splashtop.com>
2021-09-16 10:27:59 +0800

    add if no json file and break function

--- modify_json.sh
+++ modify_json.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 webhook_url=$1
-jaso_file=$2
+json_file=$2
 [ -z "$webhook_url" ] && {
   echo -e "\033[41;37mPlease set webhook url\033[0m"
+  exit
+}
+[ -z "$json_file" ] && {
+  echo -e "\033[41;37mPlease set json file name\033[0m"
   exit
 }
 num=0
@@ -11,7 +15,7 @@ while true
 do
  for n in $num
  do
-  output_warning_type=`cat $jaso_file | jq '.warnings' | jq ".[$n]"`
+  output_warning_type=`cat $json_file | jq '.warnings' | jq ".[$n]"`
   [ -z "$output_warning_type" -o "$output_warning_type" = "null" ] &&
   {
    break
@@ -22,14 +26,14 @@ do
   echo '],' >> brakeman-output-"$n".json
 
   ###update the commuit info###
-  git_project_path=`cat $jaso_file | grep app_path | awk -F'"' '{print $4}'`
+  git_project_path=`cat $json_file | grep app_path | awk -F'"' '{print $4}'`
   git_branch=`cd $git_project_path && git log -1 --pretty=format:%d | awk -F"," '{print $2}' | awk -F"origin/" '{print $2}' | awk -F")" '{print $1}'`
   git_commit_info=`cd $git_project_path && git log -1 --pretty=format:'  "git_commit": [%n    {%n      "id": "%H",%n      "message": "%s",%n      "notes": "%N",%n      "name": "%aN",%n      "email": "%aE",%n      "date": "%aD",%n      "branch": "'$git_branch'"%n    }%n  ],%n'`
   echo "$git_commit_info" >> brakeman-output-"$n".json
 
 echo $git_branch
   ###update repo info###
-  repo=`cat $jaso_file | grep app_path | awk -F'"' '{print $4}' | awk -F'/' '{print $4}'`
+  repo=`cat $json_file | grep app_path | awk -F'"' '{print $4}' | awk -F'/' '{print $4}'`
   echo '"repo_name": "'$repo'"' >> brakeman-output-"$n".json
   echo '}' >> brakeman-output-"$n".json
   curl -X POST $webhook_url -H "Content-Type:\ application/json" -d @"brakeman-output-"$n".json"
