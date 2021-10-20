52b7fedde524d1f557d122dc9efb4d4a20ee4d62
jackal.kao <jackal.kao@splashtop.com>
2021-10-20 16:54:48 +0800

    add pull request id to json data

--- modify_json.sh
+++ modify_json.sh
@@ -39,11 +39,13 @@ do
   repo=`echo $2 | awk -F"," '{print $1}'`
   git_branch=`echo $2 | awk -F"," '{print $2}'`
   git_hash=`echo $2 | awk -F"," '{print $3}'`
+  pr_id=`echo $2 | awk -F"," '{print $4}'`
   git_commit_info=`git log -1 --pretty=format:'  "git_commit": [%n    {%n      "id": "'$git_hash'",%n      "message": "%s",%n      "notes": "%N",%n      "name": "%aN",%n      "email": "%aE",%n      "date": "%aD",%n      "branch": "'$git_branch'"%n    }%n  ],%n'`
   echo "$git_commit_info" >> brakeman-output-"$n".json
   cat brakeman-output-0.json
   ###update repo info###
   echo '"repo_name": "'$repo'"' >> brakeman-output-"$n".json
+  echo '"pull_request_id": "'$pr_id'"' >> brakeman-output-"$n".json
   echo '}' >> brakeman-output-"$n".json
   curl -X POST $webhook_url -H "Content-Type:\ application/json" -d @"brakeman-output-"$n".json"
   cat brakeman-output-0.json
