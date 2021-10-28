f56279058079a98847d95f241459e9982e5fc6c3
jackal.kao <jackal.kao@splashtop.com>
2021-10-28 11:07:27 +0800

    remove echo json data info

--- modify_json.sh
+++ modify_json.sh
@@ -42,7 +42,6 @@ do
   pr_id=`echo $2 | awk -F"," '{print $4}'`
   git_commit_info=`git log -1 --pretty=format:'  "git_commit": [%n    {%n      "id": "'$git_hash'",%n      "message": "%s",%n      "notes": "%N",%n      "name": "%aN",%n      "email": "%aE",%n      "date": "%aD",%n      "branch": "'$git_branch'"%n    }%n  ],%n'`
   echo "$git_commit_info" >> brakeman-output-"$n".json
-  cat brakeman-output-0.json
   ###update repo info###
   echo '"repo_name": "'$repo'",' >> brakeman-output-"$n".json
   echo '"pull_request_id": "'$pr_id'"' >> brakeman-output-"$n".json
